28fcd51a21b8272e026975ae5c499f87f0d602a3
shikunsheng <1324281299@qq.com>
2023-06-29 11:33:25 +0800

    打印日志

--- app/Utils/WanLinYun.php
+++ app/Utils/WanLinYun.php
@@ -3,6 +3,7 @@
 namespace App\Utils;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class WanLinYun
 {
@@ -14,20 +15,25 @@ class WanLinYun
     {
         $this->client = new Client();
     }
+
     public function remoteControl($chipcode, $clientId, $runTime, $switchState)
     {
-        $response = $this->client->request('POST', self::HOST . '?act=Api&method=three_power_set', [
-            'json' => [
-                "token"     => ENV('WLY_TOKEN'),
-                "user_name" => ENV('WLY_USERNAME'),
-                "setdata"   => [
-                    "chipcode"     => $chipcode,
-                    "client_id"    => $clientId,
-                    "run_time"     => "00",
-                    "switch_state" => "01",
-                    "loop"         => "01",
-                ],
+        $json = [
+            "token"     => ENV('WLY_TOKEN'),
+            "user_name" => ENV('WLY_USERNAME'),
+            "setdata"   => [
+                "chipcode"     => $chipcode,
+                "client_id"    => $clientId,
+                "run_time"     => $runTime,
+                "switch_state" => $switchState,
+                "loop"         => "01",
             ],
+        ];
+        $timestamp = now();
+
+        Log::info("send JSON: " . json_encode($json) . " | Timestamp: {$timestamp}");
+        $response = $this->client->request('POST', self::HOST . '?act=Api&method=three_power_set', [
+            'json' => $json,
         ]);
         if ($response->getStatusCode() === 200) {
             return json_decode($response->getBody(), true);
